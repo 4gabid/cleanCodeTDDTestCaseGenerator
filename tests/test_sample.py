7fbe4da8883880728816050974c45daeeae64e1f
Gabrielle Desiree <gabrielle.desiree@gmail.com>
2025-02-16 19:00:18 -0300

    VPM 30 valid refactor+ parametrization

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,15 +1,11 @@
 import sys
 import os
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from JamaTestCaseGenerator import isVPMValid
 
-def test_validVPM10():
-    assert isVPMValid(10) == True
-
-def test_validVPM20():
-    assert isVPMValid(20) == True
-
-def test_validVPM30():
-    assert isVPMValid(30) == True
+@pytest.mark.parametrize("value", [10, 20, 30])
+def test_validVPM(value):
+    assert isVPMValid(value) == True
 
