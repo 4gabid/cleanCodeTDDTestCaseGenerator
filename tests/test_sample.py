d0b950fbc3ea99739cfedb169ff90eced313b61d
Gabrielle Desiree <gabrielle.desiree@gmail.com>
2025-02-16 19:01:31 -0300

    all valid VPMs red
    Removed 30 because it is not valid

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 
 from JamaTestCaseGenerator import isVPMValid
 
-@pytest.mark.parametrize("value", [10, 20, 30])
+@pytest.mark.parametrize("value", [10, 20, 40, 50, 60, 70])
 def test_validVPM(value):
     assert isVPMValid(value) == True
 
